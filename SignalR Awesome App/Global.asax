af0159274f3784bf249fff6c8a283f4791500ab5
João Victor Rodrigues Alves <joao.alves@new.net>
2019-03-27 17:37:50 -0300

    initial commit

---
+++ SignalR Awesome App/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SignalR_Awesome_App.MvcApplication" Language="C#" %>
---
+++ SignalR Awesome App/Scripts/SignalR.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    $.connection.hub.start()
+        .done(function () {
+            console.log("Success")
+            $.connection.myHub.server.announce("Connected!");
+        })
+        .fail(function () { alert("Fail") });
+
+    $.connection.myHub.client.announce = function (message) {
+
+        $("#welcome-messages").append(message + "<br />");
+    }
+})()
